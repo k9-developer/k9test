5473ad48740034402312e6410f7fc2f6be59dc65
k9-developer <k9@inet.ua>
2010-06-09 04:39:32 +0300

    1

--- mydata/management/printlist.py
+++ mydata/management/printlist.py
@@ -1,7 +1,5 @@
-import datetime
 from django.core.management.base import NoArgsCommand
 from django.contrib.contenttypes.models import ContentType
-from k9test.mydata.models import MyData, HttpReq
 
 class Command(NoArgsCommand):
     help = "Can be run  directly"
