d7f6f9546740d52819ed8ff02a5a48c49d00352d
k9-developer <k9@inet.ua>
2010-06-10 03:50:00 +0300

    t8

--- mydata/templatetags/get_edit_url.py
+++ mydata/templatetags/get_edit_url.py
@@ -1,7 +1,9 @@
 from django import template
 
+
 register = template.Library()
 
 @register.simple_tag
 def get_edit_url(object):
-    return "/admin/" + object._meta.app_label + "/" + object._meta.module_name + "/" + str(object.id) + "/"
+    return "/admin/" + object._meta.app_label + "/" \
+        + object._meta.module_name + "/" + str(object.id) + "/"
