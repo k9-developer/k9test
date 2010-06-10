21e3c5b66842646270cbf9a15f122ac913a49691
k9-developer <k9@inet.ua>
2010-06-10 03:53:51 +0300

    t9

--- mydata/management/printlist.py
+++ mydata/management/printlist.py
@@ -2,10 +2,9 @@ from django.core.management.base import NoArgsCommand
 from django.contrib.contenttypes.models import ContentType
 
 class Command(NoArgsCommand):
-    help = "Can be run  directly"
+    help = "Can be run directly"
 
     def handle_noargs(self, **options):
         for obj in ContentType.objects.all():
-            print "Model: %s , count - %s." % (obj.model_class(), obj.model_class().objects.count())
-
-
+            print "Model: %s , count - %s." % \
+                (obj.model_class(), obj.model_class().objects.count())
