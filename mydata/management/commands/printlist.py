1c7b5872c88f179f8baf3bafc64707606919990f
k9-developer <k9@inet.ua>
2010-06-15 13:54:00 +0300

    t9_commands

--- mydata/management/commands/printlist.py
+++ mydata/management/commands/printlist.py
@@ -1,13 +1,12 @@
 import datetime
 from django.core.management.base import NoArgsCommand
 from django.contrib.contenttypes.models import ContentType
-from k9test.mydata.models import MyData, HttpReq
+
 
 class Command(NoArgsCommand):
     help = "Can be run  directly"
 
     def handle_noargs(self, **options):
         for obj in ContentType.objects.all():
-            print "Model: %s , count - %s." % (obj.model_class(), obj.model_class().objects.count())
-
-
+            print "Model: %s , count - %s." % \
+                (obj.model_class(), obj.model_class().objects.count())
