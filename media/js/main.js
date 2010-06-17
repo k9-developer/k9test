f8844fcd40264a5df970d741921bcc9bcf12d113
k9-developer <k9@inet.ua>
2010-06-17 12:59:15 +0300

    t11_jquery fix

--- media/js/main.js
+++ media/js/main.js
@@ -1,16 +1,19 @@
- $(document).ready(function(){
+$(document).ready(function(){
 	 $('#my_data_ajax_edit').load('/ajax/mydata/edit/');
- });
+});
  
- function mydata_save(path){
-	 $('#load_msg').text("Save ....");
-	 $.post(path, $("#my_data_ajax_edit_form").serialize(),
+function mydata_save(path){
+	sendData = $("#my_data_ajax_edit_form").serialize()
+    $('form#my_data_ajax_edit_form > *').attr("disabled","disabled");
+	$('form#my_data_ajax_edit_form > * > *').attr("disabled","disabled");
+	$('#load_msg').text("Save ....");
+	$.post(path, sendData,
 		function(data, textStatus){
 		 	if (textStatus == "success"){
 		 		$('#my_data_ajax_edit_form').replaceWith(data);
 		 		$('#load_msg').text("OK");
-		 		$('form#my_data_ajax_edit_form > *').attr("disabled","disabled");
-		 		$('form#my_data_ajax_edit_form > * > *').attr("disabled","disabled");
+		 		$('form#my_data_ajax_edit_form > *').removeAttr("disabled");
+		 		$('form#my_data_ajax_edit_form > * > *').removeAttr("disabled");
 		 	} else {$('#load_msg').text("Save Error !!!");}
 		});
- }
+}
