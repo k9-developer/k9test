54acf0ab740711e1227aebfedfa6d9d85e719e41
k9-developer <k9@inet.ua>
2010-06-18 14:34:43 +0300

    добавлена подсветка ошибок в форме

--- media/js/main.js
+++ media/js/main.js
@@ -1,37 +1,41 @@
 $(document).ready(function(){
-	$.get('/ajax/mydata/edit/', function(data) {
-			$('#my_data_ajax_edit').html(data);
-			datepicker_bind();
-		});
+    $.get('/ajax/mydata/edit/', function(data) {
+            $('#my_data_ajax_edit').html(data);
+            $("td:has(ul.errorlist)").addClass('marked');
+            $("tr:has(td.marked)").addClass('red');
+            datepicker_bind();
+        });
 });
- 
+
 function mydata_save(path){
-	sendData = $("#my_data_ajax_edit_form").serialize()
+    sendData = $("#my_data_ajax_edit_form").serialize()
     $('form#my_data_ajax_edit_form > *').attr("disabled","disabled");
-	$('form#my_data_ajax_edit_form > * > *').attr("disabled","disabled");
-	$('#load_msg').text("Saving");
-	$.post(path, sendData,
-		function(data, textStatus){
-		 	if (textStatus == "success"){
-		 		$('#my_data_ajax_edit_form').replaceWith(data);
-		 		$('#load_msg').text("OK");
-		 		$('form#my_data_ajax_edit_form > *').removeAttr("disabled");
-		 		$('form#my_data_ajax_edit_form > * > *').removeAttr("disabled");
-		 		datepicker_bind();
-		 	} else {$('#load_msg').text("Error !!!"); datepicker_bind();}
-		});
-	
+    $('form#my_data_ajax_edit_form > * > *').attr("disabled","disabled");
+    $('#load_msg').text("Saving");
+    $.post(path, sendData,
+        function(data, textStatus){
+            if (textStatus == "success"){
+                $('#my_data_ajax_edit_form').replaceWith(data);
+                $('#load_msg').text("OK");
+                $('form#my_data_ajax_edit_form > *').removeAttr("disabled");
+                $('form#my_data_ajax_edit_form > * > *').removeAttr("disabled");
+                datepicker_bind();
+            } else {$('#load_msg').text("Error !!!"); datepicker_bind();}
+        });
+    
 };
 
 function datepicker_bind(){
-	$(function() {
-		currentTime = new Date();
-		yearString = '1900:'+ currentTime.getFullYear();
-	    $('input#id_birthday').datepicker({
-	        dateFormat: 'yy-mm-dd',
-	        changeMonth: true,
-	        changeYear: true,
-	        yearRange: yearString,
-	    });
-	});
+    $(function() {
+        currentTime = new Date();
+        yearString = '1900:'+ currentTime.getFullYear();
+        $('input#id_birthday').datepicker({
+            dateFormat: 'yy-mm-dd',
+            changeMonth: true,
+            changeYear: true,
+            yearRange: yearString,
+        });
+    });
+    $("td:has(ul.errorlist)").addClass('marked');
+    $("tr:has(td.marked)").addClass('red');
 };
